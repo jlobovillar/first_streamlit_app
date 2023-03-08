f6fb355990fee5c6eb6fc85d14465900fddd5c61
jlobovillar <127316951+jlobovillar@users.noreply.github.com>
2023-03-08 11:19:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,9 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Pongamos una lista de selección aquí para que puedan escoger la fruta que quieren incluir 
+streamlit.multiselect("Recoger algunas frutas:", list(my_fruit_list.index)) 
+
+# Mostrar la tabla en la página.
 streamlit.dataframe(my_fruit_list)
