cc66cb5bed9a1b75e40626af66e85a010a1bb150
jlobovillar <127316951+jlobovillar@users.noreply.github.com>
2023-03-08 11:01:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healtly Diner')
 
-streamlit.title('breakfast Menu')
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
